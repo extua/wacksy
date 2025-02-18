aaa5207e19f444e3f7d17aac5f57f83f668c3d66
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-02-18 15:58:50 +0000

    refactor: new zip writing example
    this one from https://github.com/nickbabcock/rawzip/blob/921745bbb092a1ff3ad32f3b18a488c0ba4f9ac4/examples/write.rs

--- src/zip_dir.rs
+++ src/zip_dir.rs
@@ -1,81 +1,20 @@
-use zip::{CompressionMethod,ZipWriter};
-use std::io::prelude::*;
-use zip::{result::ZipError, write::SimpleFileOptions};
+use std::{fs::File, io::Write};
 
-use std::fs::File;
-use std::path::{Path, PathBuf};
-use walkdir::{DirEntry, WalkDir};
+fn main() {
+    let buffer = File::create("foo.zip").expect("couldn't create file");
+    let mut writer = rawzip::ZipArchiveWriter::new(buffer);
+    writer.new_dir("dir/").unwrap();
 
+    let options =
+        rawzip::ZipEntryOptions::default().compression_method(rawzip::CompressionMethod::Store);
 
-
-pub fn create_zip() {
-    let src_dir = "../examples";
-    let dst_file = "example.zip";
-    let method = CompressionMethod::Stored;
-
-    match doit(src_dir, dst_file, method) {
-        Ok(_) => println!("done: {:?} written to {:?}", src_dir, dst_file),
-        Err(e) => eprintln!("Error: {e:?}"),
-    }
-}
-
-fn zip_dir<T>(
-    it: &mut dyn Iterator<Item = DirEntry>,
-    prefix: &Path,
-    writer: T,
-    method: zip::CompressionMethod,
-) -> ()
-where
-    T: Write + Seek,
-{
-    let mut zip = zip::ZipWriter::new(writer);
-    let options = SimpleFileOptions::default()
-        .compression_method(method)
-        .unix_permissions(0o755);
-
-    let prefix = Path::new(prefix);
-    let mut buffer = Vec::new();
-    for entry in it {
-        let path = entry.path();
-        let name = path.strip_prefix(prefix).unwrap();
-        let path_as_string = name
-            .to_str()
-            .map(str::to_owned)
-            .with_context(|| format!("{name:?} Is a Non UTF-8 Path"))?;
-
-        // Write file or directory explicitly
-        // Some unzip tools unzip files with directory paths correctly, some do not!
-        if path.is_file() {
-            println!("adding file {path:?} as {name:?} ...");
-            zip.start_file(path_as_string, options)?;
-            let mut f = File::open(path)?;
-
-            f.read_to_end(&mut buffer)?;
-            zip.write_all(&buffer)?;
-            buffer.clear();
-        } else if !name.as_os_str().is_empty() {
-            // Only if not root! Avoids path spec / warning
-            // and mapname conversion failed error on unzip
-            println!("adding dir {path_as_string:?} as {name:?} ...");
-            zip.add_directory(path_as_string, options)?;
-        }
-    }
-    zip.finish()?;
-    Ok(())
-}
-
-fn doit(src_dir: &Path, dst_file: &Path, method: zip::CompressionMethod) -> anyhow::Result<()> {
-    if !Path::new(src_dir).is_dir() {
-        return Err(ZipError::FileNotFound.into());
-    }
-
-    let path = Path::new(dst_file);
-    let file = File::create(path).unwrap();
-
-    let walkdir = WalkDir::new(src_dir);
-    let it = walkdir.into_iter();
-
-    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
-
-    Ok(())
+    let mut file = writer.new_file("dir/test.txt", options).unwrap();
+    let output = {
+        let mut writer = rawzip::RawZipWriter::new(&mut file);
+        writer.write_all(b"Hello, world!").unwrap();
+        let (_, output) = writer.finish().unwrap();
+        output
+    };
+    file.finish(output).unwrap();
+    writer.finish().unwrap();
 }
