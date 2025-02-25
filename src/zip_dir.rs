a7957985b138ced2639c0b77e6d6c25c7de3dbbf
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-02-25 16:47:52 +0000

    refactor: wrap up the file adding logic into its own function

--- src/zip_dir.rs
+++ src/zip_dir.rs
@@ -8,26 +8,36 @@ pub fn zip_dir(warc_file: &[u8]) -> Result<Vec<u8>, Error> {
     // Set options, with no compression.
     let options = ZipEntryOptions::default().compression_method(CompressionMethod::Store);
 
-    // Start a new file in our zip archive.
-    let mut file = archive.new_file("archive/data.warc", options).unwrap();
+    fn add_file_to_archive(
+        archive: &mut ZipArchiveWriter<&mut Vec<u8>>,
+        options: ZipEntryOptions,
+        file_data: &[u8],
+        file_path: &str,
+    ) {
+        // Start a new file in our zip archive.
+        let mut file = archive.new_file(file_path, options).unwrap();
 
-    // Wrap the file in a RawZipWriter, which will track information for the
-    // Zip data descriptor (like uncompressed size and crc).
-    let mut writer = RawZipWriter::new(&mut file);
+        // Wrap the file in a RawZipWriter, which will track information for the
+        // Zip data descriptor (like uncompressed size and crc).
+        let mut writer = RawZipWriter::new(&mut file);
 
-    // Copy the data to the writer.
-    std::io::copy(&mut &warc_file[..], &mut writer).unwrap();
+        // Copy the data to the writer.
+        std::io::copy(&mut &file_data[..], &mut writer).unwrap();
 
-    let descriptor = {
         // Finish the file, which will return the finalized data descriptor
         let (_, descriptor) = writer.finish().unwrap();
-        descriptor
-    };
 
-    // Write out the data descriptor and return the number of bytes the data compressed to.
-    let compressed = file.finish(descriptor)?;
+        let uncompressed_size = descriptor.uncompressed_size();
 
-    println!("wrote {compressed} bytes");
+        println!("wrote {uncompressed_size} bytes to {file_path}");
+
+        // Write out the data descriptor and return the number of bytes the data compressed to.
+        file.finish(descriptor).unwrap();
+    }
+
+    let file_path: &str = "archive/data.warc";
+
+    add_file_to_archive(&mut archive, options, warc_file, file_path);
 
     // Finish the archive, which will write the central directory.
     archive.finish()?;
