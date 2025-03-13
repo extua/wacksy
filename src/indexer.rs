db47c81ad149a2a46015d96437b0f80d052742b2
Pierre Marshall <pierre.marshall@gmail.com>
2025-03-13 12:38:10 +0000

    syle: fix pedantic clippy lints
    and comment the create_searchable_url fn

--- src/indexer.rs
+++ src/indexer.rs
@@ -1,5 +1,4 @@
-use std::borrow::Cow;
-use std::error::Error;
+use core::error::Error;
 use std::path::Path;
 
 use url::Position;
@@ -7,40 +6,45 @@ use url::Url;
 use warc::WarcHeader;
 use warc::WarcReader;
 
-fn create_searchable_url(
-    url: Cow<'_, str>,
-) -> Result<String, Box<dyn Error + Send + Sync + 'static>> {
-    // Cow to String
-    let lowercase_url = url.as_ref().to_lowercase();
-    let parsed_url = Url::parse(&lowercase_url);
-    match parsed_url {
-        Err(err) => Err(format!("Error parsing URL: {err}\r\n").into()),
-        Ok(parsed_url) => {
-            if let Some(host) = parsed_url.host_str() {
-                let mut host_split: Vec<&str> = host.split('.').collect();
-                host_split.reverse();
-                let host_reversed = host_split.join(",");
-                let url_path = &parsed_url[Position::BeforePath..];
-                let searchable_url = format!("{host_reversed}){url_path}");
-                Ok(searchable_url)
-            } else {
-                Err(format!("No hostname found in {lowercase_url}, handle this error!\r\n").into())
+pub fn compose_index(warc_file_path: &Path) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    fn create_searchable_url(
+        url: &str,
+    ) -> Result<String, Box<dyn Error + Send + Sync + 'static>> {
+        let lowercase_url = url.to_lowercase();
+        let parsed_url = Url::parse(&lowercase_url);
+        match parsed_url {
+            Err(err) => return Err(format!("Error parsing URL: {err}\r\n").into()),
+            Ok(successfully_parsed_url) => {
+                if let Some(host) = successfully_parsed_url.host_str() {
+                    // split the host string into an array at each dot
+                    let mut host_split: Vec<&str> = host.split('.').collect();
+                    // reverse the order of the array
+                    host_split.reverse();
+                    // join the array back into a comma-separated string
+                    let host_reversed = host_split.join(",");
+                    // capture everything else on the end of the url
+                    let url_path = &successfully_parsed_url[Position::BeforePath..];
+                    // put it all together
+                    let searchable_url = format!("{host_reversed}){url_path}");
+                    return Ok(searchable_url)
+                }
+                return Err(format!("No hostname found in {lowercase_url}, handle this error!\r\n").into())
             }
         }
     }
-}
 
-pub fn compose_index(warc_file_path: &Path) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     let file = WarcReader::from_path_gzip(warc_file_path)?;
 
-    let mut count = 0;
+    let mut count: usize = 0;
     for record in file.iter_records() {
+        // counting arithmetic is unsafe
+        // do something about this in future
         count += 1;
         match record {
-            Err(err) => println!("ERROR: {}\r\n", err),
+            Err(err) => println!("ERROR: {err}\r\n"),
             Ok(record) => {
                 if let Some(url) = record.header(WarcHeader::TargetURI) {
-                    let searchable_url = create_searchable_url(url)?;
+                    let searchable_url = create_searchable_url(&url)?;
                     println!("{searchable_url}");
                 } else {
                     println!("No url found in record, handle this error!");
