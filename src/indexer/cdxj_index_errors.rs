ffe30b948d63af4bade356ab76b37be736bd7b5d
Pierre <66693681+extua@users.noreply.github.com>
2025-05-07 09:48:07 +0000

    style: replace CDXJIndexRecordError with Self
    clippy warning about unnecessary structure name repetition

--- src/indexer/cdxj_index_errors.rs
+++ src/indexer/cdxj_index_errors.rs
@@ -12,22 +12,22 @@ pub enum CDXJIndexRecordError {
 impl std::fmt::Display for CDXJIndexRecordError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            CDXJIndexRecordError::RecordTimestampError(parse_error_message) => {
+            Self::RecordTimestampError(parse_error_message) => {
                 write!(f, "Could not get record timestamp: {parse_error_message}")
             }
-            CDXJIndexRecordError::WarcFilenameError(error_message) => {
+            Self::WarcFilenameError(error_message) => {
                 write!(f, "Could not get WARC filename: {error_message}")
             }
-            CDXJIndexRecordError::RecordContentTypeError(error_message) => {
+            Self::RecordContentTypeError(error_message) => {
                 write!(f, "Could not parse record content type: {error_message}")
             }
-            CDXJIndexRecordError::RecordUrlError(parse_error_message) => {
+            Self::RecordUrlError(parse_error_message) => {
                 write!(f, "Could not parse url: {parse_error_message}")
             }
-            CDXJIndexRecordError::RecordStatusError(parse_int_error_message) => {
+            Self::RecordStatusError(parse_int_error_message) => {
                 write!(f, "Could not parse HTTP status: {parse_int_error_message}")
             }
-            CDXJIndexRecordError::ValueNotFound(error_message) => {
+            Self::ValueNotFound(error_message) => {
                 write!(f, "Value not found: {error_message}")
             }
         }
@@ -36,12 +36,12 @@ impl std::fmt::Display for CDXJIndexRecordError {
 impl Error for CDXJIndexRecordError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            CDXJIndexRecordError::RecordTimestampError(parse_error) => Some(parse_error),
-            CDXJIndexRecordError::RecordUrlError(parse_error) => Some(parse_error),
-            CDXJIndexRecordError::RecordStatusError(parse_int_error) => Some(parse_int_error),
-            CDXJIndexRecordError::ValueNotFound(_)
-            | CDXJIndexRecordError::RecordContentTypeError(_)
-            | CDXJIndexRecordError::WarcFilenameError(_) => None,
+            Self::RecordTimestampError(parse_error) => Some(parse_error),
+            Self::RecordUrlError(parse_error) => Some(parse_error),
+            Self::RecordStatusError(parse_int_error) => Some(parse_int_error),
+            Self::ValueNotFound(_)
+            | Self::RecordContentTypeError(_)
+            | Self::WarcFilenameError(_) => None,
         }
     }
 }
