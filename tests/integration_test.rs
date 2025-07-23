77cbfe42017f62d90a6d2a1b7c2ed62659cc638a
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-07-23 15:19:56 +0100

    test(indexer): add integration test for pages file
    also move indexer into mod.rs

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,13 +1,47 @@
 use std::{fs, path::Path};
+use wacksy::*;
 
 const WARC_PATH: &str = "tests/rec-e7e68da067d0-20250423121042981-0.warc.gz";
 
 #[test]
-fn test_cdxj_indexer() -> Result<(), std::io::Error> {
+fn create_cdxj_index() -> Result<(), std::io::Error> {
     let warc_file_path: &Path = Path::new(WARC_PATH);
-    let index = wacksy::indexer::index_file(warc_file_path)?;
+    let index = indexer::index_file(warc_file_path)?;
     let generated_cdxj_index = index.0.to_string();
-    let example_cdxj_index = fs::read_to_string(Path::new("tests/index.cdxj"))?;
+    let example_cdxj_index =
+        fs::read_to_string(Path::new("tests/wacz_example/indexes/index.cdxj"))?;
     assert_eq!(generated_cdxj_index, example_cdxj_index);
     Ok(())
 }
+
+#[test]
+fn create_pages_index() -> Result<(), std::io::Error> {
+    let warc_file_path: &Path = Path::new(WARC_PATH);
+    let index = indexer::index_file(warc_file_path)?;
+    let generated_pages_index = index.1.to_string();
+    let example_pages_index =
+        fs::read_to_string(Path::new("tests/wacz_example/pages/pages.jsonl"))?;
+    assert_eq!(generated_pages_index, example_pages_index);
+    Ok(())
+}
+
+// the datapackage cannot be easily tested because it contains
+// a local timestamp, how do I mock this?
+// #[test]
+// fn create_datapackage() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+//     let warc_file_path: &Path = Path::new(WARC_PATH);
+//     let index = indexer::index_file(warc_file_path)?;
+//     let warc_file = fs::read(warc_file_path)?;
+
+//     let cdxj_index_bytes = index.0.to_string().into_bytes();
+//     let pages_index_bytes = index.1.to_string().into_bytes();
+
+//     let data_package =
+//         datapackage::DataPackage::new(&warc_file, &cdxj_index_bytes, &pages_index_bytes)?;
+
+//     let generated_data_package = serde_json::to_string(&data_package)?;
+//     let example_data_package =
+//         fs::read_to_string(Path::new("tests/wacz_example/datapackage.json"))?;
+//     assert_eq!(generated_data_package, example_data_package);
+//     Ok(())
+// }
