b26700c60ad974614b44e3412244d163f18375ab
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-08-29 11:24:35 +0100

    fix(indexer): add title to pages.jsonl header, closing #51 (again)
    this (should) pass frictionless data package validation

--- src/indexer.rs
+++ src/indexer.rs
@@ -151,7 +151,7 @@ pub struct PageIndex(Vec<PageRecord>);
 impl fmt::Display for PageIndex {
     fn fmt(&self, message: &mut fmt::Formatter) -> fmt::Result {
         let index_string: String = self.0.iter().map(ToString::to_string).collect();
-        let header: &'static str = "{\"format\":\"json-pages-1.0\",\"id\":\"pages\"}";
+        let header: &'static str = "{\"format\":\"json-pages-1.0\",\"id\":\"pages\",\"title\":\"All Pages\"}";
         return write!(message, "{header}\n{index_string}");
     }
 }
